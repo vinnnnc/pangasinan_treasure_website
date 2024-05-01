80a735931d8c4ce047eb0d7e3f71bf2b3c5e34f8
Vincent <vincentbautista.123@gmail.com>
2024-05-01 17:27:58 +0800

    modified:   public/assets/css/profile.css 	modified:   public/assets/css/styles.css 	new file:   public/assets/images/category/food_category.png 	new file:   public/assets/images/category/handicrafts_category.png 	new file:   public/assets/images/category/local_category.png 	new file:   public/assets/images/category/souvenirs_category.png 	new file:   public/assets/images/category/sweets_category.png 	new file:   public/assets/images/category/textiles_category.png 	deleted:    public/assets/images/products/Picture3.png 	modified:   public/assets/script/profile.js 	modified:   public/index.html 	modified:   public/profile.html 	modified:   public/profile/profile_address.html 	modified:   public/profile/profile_edit.html 	modified:   public/profile/profile_orders.html 	modified:   public/profile/profile_payment.html 	new file:   public/profile/profile_wallet.html

--- public/assets/script/profile.js
+++ public/assets/script/profile.js
@@ -1,27 +1,77 @@
-function loadEditProfile() {
-  $(".right-section").load("/profile/profile_edit.html");
-}
-function loadAddress() {
-  $(".right-section").load("/profile/profile_address.html");
-}
-function loadCancellations() {
-  $(".right-section").load("/profile/profile_cancellations.html");
-}
-function loadOrders() {
-  $(".right-section").load("/profile/profile_orders.html");
-}
-function loadPayment() {
-  $(".right-section").load("/profile/profile_payment.html");
-}
-function loadReturns() {
-  $(".right-section").load("/profile/profile_returns.html");
-}
+const profileLinks = document.querySelectorAll(".profile-links div");
+const rightSection = document.querySelector(".right-section");
 
-const links = document.querySelectorAll(".left-section a");
-
-links.forEach((link) => {
+profileLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    links.forEach((l) => l.classList.remove("active"));
-    link.classList.add("active");
+    const html = getHtmlPath(link.id);
+    loadContent(html, link);
+    setTitle(link.classList.contains("orders-link") ? 2 : 1);
   });
 });
+
+function getHtmlPath(linkId) {
+  switch (linkId) {
+    case "manage-account":
+    case "my-account-title":
+      return "profile_edit.html";
+    case "payment-options":
+      return "profile_payment.html";
+    case "address-book":
+      return "profile_address.html";
+    case "my-wallet":
+      return "profile_wallet.html";
+    case "my-orders":
+    case "my-orders-title":
+      return "profile_orders.html";
+    case "my-returns":
+      return "profile_returns.html";
+    case "my-cancellations":
+      return "profile_cancellations.html";
+    default:
+      return "";
+  }
+}
+
+function loadContent(html, link) {
+  profileLinks.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  rightSection.innerHTML = ""; // Clear previous content
+  rightSection.insertAdjacentHTML(
+    "beforeend",
+    `<div class="loading">Loading...</div>`
+  );
+  fetch(`/profile/${html}`, {
+    headers: {
+      Accept: "text/html",
+    },
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      rightSection.innerHTML = data;
+      link.classList.add("active");
+      if (link.id === "my-account-title")
+        document.getElementById("manage-account").classList.add("active");
+      else if (link.id === "my-orders-title")
+        document.getElementById("my-orders").classList.add("active");
+    })
+    .catch((error) => console.error("Error loading content:", error));
+}
+
+function setTitle(num) {
+  profileLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+  document
+    .getElementById(num === 1 ? "my-account-title" : "my-orders-title")
+    .classList.add("active");
+}
+
+function loadDefault() {
+  loadContent(
+    "profile_orders.html",
+    document.querySelector(".profile-links div:first-child")
+  );
+  document.getElementById("my-account-title").classList.add("active");
+}
