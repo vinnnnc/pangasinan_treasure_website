005afbee16b649c59b1acea109f82c534f33d678
Vincent <vincentbautista.123@gmail.com>
2024-05-12 05:15:47 +0800

    deleted:    .env 	modified:   public/assets/script/script.js

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -151,7 +151,24 @@ function fetchProducts() {
     });
 }
 
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
 function createProductCards(data) {
+  shuffle(data);
   // Data contains the products array
   const productList = document.getElementById("products");
 
