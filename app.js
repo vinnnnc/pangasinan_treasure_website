4d7790fce659ee0322f6e552d3fd76c5b9f9e8b8
Vincent <vincentbautista.123@gmail.com>
2024-05-14 09:51:47 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.js
    	modified:   public/about.html
    	modified:   public/assets/css/about.css
    	new file:   public/assets/css/contact.css
    	modified:   public/assets/css/footer.css
    	modified:   public/assets/css/styles.css
    	new file:   public/assets/images/icons/comments-solid.svg
    	new file:   public/assets/images/icons/phone-solid.svg
    	new file:   public/contact.html
    	modified:   public/footer.html
    	modified:   public/header.html

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.get("/admin", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/public/about.html");
 });
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/public/contact.html");
+});
 app.get("/cart", (req, res) => {
   res.sendFile(__dirname + "/public/cart.html");
 });
